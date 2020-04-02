e805ec51c5cd4163f7135896f7d6e94d71e976f9
carolinabranquinho <carol.a.branquinho@gmail.com>
2020-04-02 11:38:55 -0300

    Change the fog credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
         # Configuration for Amazon S3
         :provider              => 'AWS',
         :aws_access_key_id     => ENV['AKIAJLRKXHHR53TD5BHA'],
-        :aws_secret_access_key => ENV['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']
-        :region => ENV['sa-east-1']
+        :aws_secret_access_key => ENV['CXCGAsQDqFKp5cjM1wUOMJUio+2pNmj+IuOkplto'],
+        :region                => ENV['sa-east-1']
       }
       config.fog_directory     =  ENV['sampleappbucket2020']
     end
