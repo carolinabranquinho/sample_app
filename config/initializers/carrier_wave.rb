69eb0dada7b8e755fca67c021c3a2fd2aba1ee6d
carolinabranquinho <carol.a.branquinho@gmail.com>
2020-04-02 11:00:33 -0300

    Change region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
         :provider              => 'AWS',
         :aws_access_key_id     => ENV['AKIAJLRKXHHR53TD5BHA'],
         :aws_secret_access_key => ENV['CXCGAsQDqFKp5cjM1wUOMJUio+2pNmj+IuOkplto']
+        :region => ENV['sa-east-1']
       }
       config.fog_directory     =  ENV['sampleappbucket2020']
     end
