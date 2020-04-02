00d7f933ce9a859c02dcfe25eb8408643c6b2571
carolinabranquinho <carol.a.branquinho@gmail.com>
2020-04-02 10:27:10 -0300

    Add user microposts

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.fog_credentials = {
+        # Configuration for Amazon S3
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['AKIAJLRKXHHR53TD5BHA'],
+        :aws_secret_access_key => ENV['CXCGAsQDqFKp5cjM1wUOMJUio+2pNmj+IuOkplto']
+      }
+      config.fog_directory     =  ENV['sampleappbucket2020']
+      config.fog_provider = 'fog/aws'
+    end
+  end
